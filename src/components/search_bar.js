4f8b81d985fd5e365656ad74fa04339d7af607a7
Bryan Clover <bclover09@gmail.com>
2016-09-14 01:19:48 -0400

    Completed Section 2.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,11 +9,19 @@ class SearchBar extends Component {
 
   render() { 
     return (
-     <div>
-       <input onChangei={event => this.setState({ term: event.target.value })} />
+     <div className="search-bar">
+       <input 
+         value={this.state.term}
+         onChange={event => this.onInputChange(event.target.value)} />
      </div>
     );
   }
+  
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
 }
 
 export default SearchBar; 
